e3d48a278983e9749d3478b306ccf4fd38a12295
WINDOWS\q5081793 <jlc-1996@live.co.uk>
2018-12-03 16:12:50 +0000

    Initial Solution - Includes Layout and some User controls

---
+++ Sentry/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace Sentry
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private NotifyIcon _notifyIcon;
+        private SentryDebug _sDebug;
+        public MainWindow()
+        {
+            Setup();
+            InitializeComponent();
+        }
+
+        private void Setup()
+        {
+            _sDebug = new SentryDebug();
+            _notifyIcon = new NotifyIcon();
+            _notifyIcon.BalloonTipText = "Sentry: Small Step Studios";
+            _notifyIcon.BalloonTipTitle = "The app";
+            _notifyIcon.Text = "The app";
+            _notifyIcon.Icon = new System.Drawing.Icon("SentryLogo.ico");
+            _notifyIcon.Click += new System.EventHandler(notifyIcon_Click);
+            _notifyIcon.Visible = true;
+        }
+
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            this.Activate();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            _notifyIcon.Dispose();
+            _notifyIcon = null;
+
+            _sDebug.FlushToLog();
+            Close();
+        }
+    }
+}
---
+++ Sentry/SentryDebug.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentry
+{
+    public class SentryDebug
+    {
+        private List<string> _outputList;
+        private string _AppName = Sentry.App.ResourceAssembly.GetName().ToString();
+
+        public SentryDebug()
+        {
+            _outputList = new List<string>();
+
+            Console.WriteLine("Instanciated Sentry.exe");
+            Console.WriteLine(_AppName + " " +  DateTime.Now);
+        }
+
+        public void FlushToLog()
+        {
+            //Print _OutputList to log file for this session 
+        }
+        
+        private void Debug(string outputString)
+        {
+            WriteToConsole(DateTime.Now + ":  " +  outputString);
+        }
+
+        /// <summary>
+        /// Stores all outputs to then be flushed to a session file once the instance is closed
+        /// </summary>
+        /// <param name="outputString"></param>
+        private void WriteToConsole(string outputString)
+        {
+            Console.WriteLine(outputString);
+            _outputList.Add(outputString);
+        }
+    }
+}
